5974c55ac552e27aa74f812d428ed33cda0cecf8
Klaudia Przespolewska <klaudia1000@onet.eu>
2016-12-11 23:18:19 +0100

    www

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $("button").click(function() {
+        alert("UWAGA");
+});
+
+
+$(function animacja() {
+    $('.container').animate({"height":"100px","width": "100px", "margin-left": "100px"},3000, animacja);
+    });
+});
+
+
+/*$(function() {
+ $(".container").animate({"background-color":"blue"}, 500,animacja);
+ $('h2').fadeOut(3000).fadeIn(3000);
+    });*/
